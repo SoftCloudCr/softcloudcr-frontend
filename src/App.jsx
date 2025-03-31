1016645681f72e073e9b531c4bb4f08fee41985a
SoftCloudCr <softcloudcr25@gmail.com>
2025-03-30 21:24:10 -0600

    Se crea la base para el front end de SoftCloudCr

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+//import TestTailwind from './components/TestTailwind'
+
+export default function App() {
+  return (
+    <div className="bg-blue-800 h-screen  flex justify-center items-center">
+      <h1 className="text-blue text-3xl font-bold">
+        Ahora SÍ debería funcionar 🚀
+      </h1>
+
+      <div className="bg-green-500 text-white p-5 text-xl">
+      ¡Esto sí tiene que funcionar!
+    </div>
+    <div className="text-red-500">Texto negro</div>
+
+    </div>
+    
+  );
+}
+
---
+++ src/components/TestTailwind.jsx
@@ -0,0 +1,8 @@
+export default function TestTailwind() {
+    return (
+      <div className="bg-red-500 text-white p-5 text-2xl text-center">
+        Si ves esto en rojo, Tailwind funciona 🎯
+      </div>
+    )
+  }
+  
